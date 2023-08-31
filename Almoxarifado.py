daac464f57e3f981908f8989925d95d4be13105a
matheus coelho <matheuscmr98@gmail.com>
2023-08-31 09:35:39 -0300

    modified:   Almoxarifado.py 	modified:   Fornecedor.py

--- Almoxarifado.py
+++ Almoxarifado.py
@@ -5,31 +5,37 @@ def enviar_material(client, almoxarifado):
     print("Função enviar material executada!")
     quantidade = almoxarifado.get_estoque()
     almoxarifado.remove_estoque(quantidade)
-    mensagem = "envio "+ str(quantidade)
+    mensagem = f"envio {quantidade}"
     print(mensagem)
     client.publish("fabrica/almoxarifado", mensagem)
     almoxarifado.set_fprodutos(almoxarifado.get_fprodutos() - quantidade)
 
-def solicita_fornecedor(client, qdd):
+def solicita_fornecedor(client, almoxarifado, qdd):
     print("Função solicitar fornecedor executada!")
-    mensagem = "fornecedor "+ qdd
+    mensagem = f"fornecedor {qdd}"
     print(mensagem)
-    client.publish("fabrica/almoxarifado", mensagem)
+    client.publish("almoxarifado/fornecedor", mensagem)  # Tópico para solicitar material ao fornecedor
 
 def on_connect(client, userdata, flags, rc):
+    print("Conectado com sucesso!")
     client.subscribe("fabrica/almoxarifado")
+    client.subscribe("fornecedor/almoxarifado/resposta")  # Tópico para receber resposta do fornecedor
 
 def on_message(client, userdata, message):
     almoxarifado = userdata
     print(f"Mensagem recebida: {message.payload.decode()} no tópico {message.topic}")
-    mensagem = message.payload.decode()
-    tipo, quantidade = mensagem.split()
+    mensagem, quantidade = message.payload.decode().split()
     quantidade = int(quantidade)
-    if tipo == "solicita":
+
+    if mensagem == "solicita":
         if almoxarifado.get_estoque() >= quantidade:
             enviar_material(client, almoxarifado)
         else:
-            solicita_fornecedor(client, 3 + almoxarifado.get_fprodutos)
+            solicita_fornecedor(client, almoxarifado, 3 + almoxarifado.get_fprodutos)
+
+    elif mensagem == "envio":  # Esta é a resposta do fornecedor
+        almoxarifado.add_estoque(quantidade)
+        print(f"Adicionado ao estoque: {quantidade}")
 
 # Configuração básica
 almoxarifado = Almoxarifado(1)
@@ -37,7 +43,7 @@ broker_address = "localhost"
 port = 1883
 almoxarifado.add_estoque(10)
 
-client = mqtt.Client(userdata=almoxarifado)  # Passar almoxarifado como userdata
+client = mqtt.Client(userdata=almoxarifado)
 client.on_connect = on_connect
 client.on_message = on_message
 
--- Fornecedor.py
+++ Fornecedor.py
@@ -1,47 +1,28 @@
-# import paho.mqtt.client as mqtt
-
-# Configuração do cliente
-# client = mqtt.Client()
-# client.connect("localhost", 1883, 60)  # Conectando ao broker em localhost na porta padrão 1883
-
-# Publica uma mensagem
-# client.publish("meu/topico", "Olá, MQTT!")
-# Desconecta
-# client.disconnect()
-
-from ClasseFornecedor import Fornecedor
-
 import paho.mqtt.client as mqtt
 
-# Callback quando conectado.
-def on_connect(client, userdata, flags, rc):
-    print("Conectado com o código:", rc)
-    client.subscribe("test/topic")
+# Assumindo que o fornecedor tenha recursos ilimitados para enviar para o almoxarifado.
+def envia_material_para_almoxarifado(client, quantidade):
+    print(f"Enviando {quantidade} materiais para o almoxarifado!")
+    client.publish("fornecedor/almoxarifado/resposta", f"envio {quantidade}")
 
-# Callback quando uma mensagem é recebida.
+def on_connect(client, userdata, flags, rc):
+    print("Conectado com sucesso!")
+    client.subscribe("almoxarifado/fornecedor")
+
 def on_message(client, userdata, message):
-    print(
-        f"Mensagem recebida: {message.payload.decode()} no tópico {message.topic}")
-    mensagem = message.payload.decode()
-    tipo, quantidade = mensagem.split()
+    print(f"Mensagem recebida: {message.payload.decode()} no tópico {message.topic}")
+    mensagem, quantidade = message.payload.decode().split()
     quantidade = int(quantidade)
 
-    #Com essa mensagem, o fornecedor tem que enviar o produto pro almoxarifado
+    if mensagem == "fornecedor":
+        envia_material_para_almoxarifado(client, quantidade)
 
-    #TODO
-
-
-# Configuração básica
-fornecedor = Fornecedor(1)
-fornecedor.liberar_produto(1, 10)
 broker_address = "localhost"
 port = 1883
 
-# O nome foi alterado para refletir que ele pode ser tanto publisher quanto subscriber.
-client = mqtt.Client("Client1")
+client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
 client.connect(broker_address, port)
-
 client.loop_forever()
